46b043895070d08e981ed3955dc47d16172a5538
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-09-20 13:01:42 -0500

    Drag a image from a folder works, but not works for a browser

--- DragImageHere/ContentView.swift
+++ DragImageHere/ContentView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+  @State private var image: NSImage?
+  
+  var body: some View {
+    VStack {
+      if let image = image {
+        Image(nsImage: image)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+      } else {
+        Text("Drag and drop an image here.")
+          .font(.headline)
+          .padding()
+          .frame(maxWidth: .infinity, maxHeight: .infinity)
+          .background(Color.gray.opacity(0.3))
+      }
     }
+    .onDrop(of: ["public.file-url"], isTargeted: nil) { providers in
+      print("grace drop 1")
+      
+      guard let provider = providers.first else { return false }
+      
+      print("grace drop 2")
+      
+      provider.loadObject(ofClass: NSURL.self) { object, error in
+        if let error = error {
+          print("Error loading dropped item: \(error.localizedDescription)")
+        } else if let url = object as? URL,
+                  let data = try? Data(contentsOf: url),
+                  let droppedImage = NSImage(data: data) {
+          DispatchQueue.main.async {
+            self.image = droppedImage
+          }
+        }
+      }
+      
+      return true
+    }
+  }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
