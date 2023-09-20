7804af262dbf9aec0aa950378365d4dea2524b11
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-09-20 14:54:48 -0500

    Refactor

--- DragImageHere/ContentView.swift
+++ DragImageHere/ContentView.swift
@@ -1,11 +1,7 @@
-//
-//  ContentView.swift
-//  DragImageHere
-//
-//  Created by Le Huang on 9/20/23.
-//
-
 import SwiftUI
+import UniformTypeIdentifiers
+
+let types: [UTType] = [.fileURL, .url]
 
 struct ContentView: View {
   @State private var image: NSImage?
@@ -25,33 +21,33 @@ struct ContentView: View {
           .background(Color.gray.opacity(0.3))
       }
     }
-    .onDrop(of: ["public.file-url"], isTargeted: nil) { providers in
-      print("grace drop 1")
-      
-      guard let provider = providers.first else { return false }
-      
-      print("grace drop 2")
-      
-      provider.loadObject(ofClass: NSURL.self) { object, error in
-        if let error = error {
-          print("Error loading dropped item: \(error.localizedDescription)")
-        } else if let url = object as? URL,
-                  let data = try? Data(contentsOf: url),
-                  let droppedImage = NSImage(data: data) {
-          DispatchQueue.main.async {
-            self.image = droppedImage
-          }
+    .onDrop(of: types, isTargeted: nil, perform: performDrop)
+  }
+  
+  func performDrop(providers: [NSItemProvider]) -> Bool {
+    print("Drop detected")
+    
+    guard let provider = providers.first else { return false }
+    
+    provider.loadObject(ofClass: NSURL.self) { object, error in
+      if let error = error {
+        print("Error loading dropped item: \(error.localizedDescription)")
+      } else if let url = object as? URL,
+                let data = try? Data(contentsOf: url),
+                let droppedImage = NSImage(data: data) {
+        DispatchQueue.main.async {
+          self.image = droppedImage
         }
       }
-      
-      return true
     }
+    
+    return true
   }
 }
 
-
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+
