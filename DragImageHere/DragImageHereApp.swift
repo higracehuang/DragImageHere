c9d71efb38e219431224de2a54d4c448e6e47249
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-09-20 12:14:17 -0500

    Initial Commit

---
+++ DragImageHere/DragImageHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragImageHereApp.swift
+//  DragImageHere
+//
+//  Created by Le Huang on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DragImageHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
